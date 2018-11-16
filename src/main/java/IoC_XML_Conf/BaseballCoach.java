0654d0c55ea77ffbbfb6f64ea29c3a8df36d19b0
PiotrGry <piotrGryzlo@gmail.com>
2018-11-16 17:43:42 +0100

    Implemented Coach interface in BaseballCoach class

--- src/main/java/IoC_XML_Conf/BaseballCoach.java
+++ src/main/java/IoC_XML_Conf/BaseballCoach.java
@@ -1,4 +1,8 @@
 package IoC_XML_Conf;
 
-public class BaseballCoach {
+public class BaseballCoach implements Coach {
+
+    public String getDailyWorkout(){
+        return "Spend 30 min on batting practice";
+    }
 }
