59204c43f9a21ca13dd9b5d84daa30eb222305c4
PiotrGry <piotrGryzlo@gmail.com>
2018-11-16 18:51:53 +0100

    Added grtDailyFortune method and constructor

--- src/main/java/IoC_XML_Conf/BaseballCoach.java
+++ src/main/java/IoC_XML_Conf/BaseballCoach.java
@@ -1,8 +1,20 @@
 package IoC_XML_Conf;
 
+import DI_XML_Conf.FortuneService;
+
+
 public class BaseballCoach implements Coach {
+     private FortuneService fortuneService;
+
+    public BaseballCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
 
     public String getDailyWorkout(){
         return "Spend 30 min on batting practice";
     }
+
+   public String getDailyFortune(){
+      return fortuneService.getFortune();
+   }
 }
