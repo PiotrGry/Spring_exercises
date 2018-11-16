b6c1a011c343970fcfd918ac406d244449b00cab
PiotrGry <piotrGryzlo@gmail.com>
2018-11-16 19:17:50 +0100

    Added getDailyFortune method and constructor in TennisCoach class

--- src/main/java/IoC_XML_Conf/TennisCoach.java
+++ src/main/java/IoC_XML_Conf/TennisCoach.java
@@ -1,7 +1,19 @@
 package IoC_XML_Conf;
 
+import DI_XML_Conf.FortuneService;
+
 public class TennisCoach implements Coach {
+    FortuneService fortuneService;
+
+    public TennisCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
     public String getDailyWorkout() {
         return "Do some tennis staff";
+    }
+
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
     }
 }
