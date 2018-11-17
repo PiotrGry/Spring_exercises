0d571ff3bfe38584186173ee1982552b40ad9361
PiotrGry <piotrgryzlo@gmail.com>
2018-11-17 13:53:34 +0100

    Created setters and getters in CricketCoach

--- src/main/java/XML_Conf/DI_set_values_from_file/CricketCoach.java
+++ src/main/java/XML_Conf/DI_set_values_from_file/CricketCoach.java
@@ -1,4 +1,24 @@
 package XML_Conf.DI_set_values_from_file;
 
 public class CricketCoach {
+
+
+    private String email;
+    private String team;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
 }
